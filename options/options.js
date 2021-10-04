5d1f47a98acfd05eaf88698373a031d7ed6017b5
André Machado <csantos.machado@gmail.com>
2021-10-04 12:52:36 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "tstorderoftask1123_22848",
+  url: "https://tstorderoftask1123_22848.botics.co",
+  api: "https://tstorderoftask1123_22848.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
